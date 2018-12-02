d576002f45573c7e5fd02ce220976133ec433bf3
Janzen Go <30427833+kinggauze@users.noreply.github.com>
2018-12-02 21:08:06 +0800

    Update and rename program1 to program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,17 @@
+```c++
+#include<iostream>
+#include<stdio.h>
+#include<Windows.h>
+#include<string>
+using namespace std;
+	int main() {
+
+		string name = "";
+		cout<<"Enter your full name:";
+		cin>>name;cout<<endl;
+		cout<<name;
+		system("pause");
+		return 0;
+
+	}
+  ```
