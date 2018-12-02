da085a86ed3c417ef6d3828f3ffe0558b3a3464f
Janzen Go <30427833+kinggauze@users.noreply.github.com>
2018-12-02 21:17:22 +0800

    Create program3.cpp

---
+++ program3.cpp
@@ -0,0 +1,13 @@
+/*
+Create a program that will display
+1 2 3 4 5
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	cout<<"1\t"<<"2\t"<<"3\t"<<"4\t"<<"5\t"<<endl;
+	system("pause");
+	return 0;
+
+}
